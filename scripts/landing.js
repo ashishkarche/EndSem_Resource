aeac65cd15dd0cf9c7dfc8a130f66216c7026485
ashishkarche <ashishkarche9@gmail.com>
2024-05-20 10:32:30 +0530

    FV2.1

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,5 +1,28 @@
- // JavaScript for scrolling animation
- document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
+    const animatedText = document.getElementById("animated-text");
+    const words = ["Resource", "Question Paper", "Learning Materials", "Other resources"]; // Add your desired words here
+    let index = 0;
+
+    function changeWord() {
+        animatedText.textContent = words[index];
+        index = (index + 1) % words.length;
+        updateFontColor();
+    }
+
+    setInterval(changeWord, 5000); // Change word every 5 seconds
+
+    function updateFontColor() {
+        const gradientColors = [
+            "linear-gradient(to right, #ff8a00, #da1b60)", // Gradient color for "Resource"
+            "linear-gradient(to right, #2bc0e4, #eaecc6)", // Gradient color for "Question Paper"
+            "linear-gradient(to right, #7f00ff, #e100ff)", // Gradient color for "Learning Materials"
+            "linear-gradient(to right, #0083b0, #00b4db)"  // Gradient color for "Tools"
+        ];
+        animatedText.style.background = gradientColors[index];
+        animatedText.style.webkitBackgroundClip = "text"; // Clip the text to the gradient
+        animatedText.style.webkitTextFillColor = "transparent"; // Make the text transparent
+    }
+
     const fadeInElements = document.querySelectorAll('.fade-in-up');
 
     function fadeInOnScroll() {
